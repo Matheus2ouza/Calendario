8168b2e3c3a31d399f1ecb181f7fb08c5aad2d97
Matheus2ouza <matifurtado0@gmail.com>
2024-07-11 18:36:32 -0300

    atualização

---
+++ src/events.js
@@ -0,0 +1,8 @@
+const inputData = document.querySelectorAll(".date_event")
+
+let inputdate = new Date(),
+inputYear = date.getFullYear(),
+inputMonth =date.getMonth(),
+inputDay =date.getDay();
+
+console.log(`${inputYear}$`)
